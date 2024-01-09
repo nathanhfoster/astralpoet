56001a1d7c2c5380e3619ada1748f44e2474f948
Nathan <nfoster@yieldmo.com>
2024-01-08 23:18:52 -0500

    Calendar

---
+++ components/Calendar.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import { Calendar as RewindCalendar } from '@rewind-ui/core'
+
+interface CalendarProps {
+
+}
+const Calendar: FC<CalendarProps> = () => {
+
+    return <RewindCalendar></RewindCalendar>
+}
+
+export default Calendar
